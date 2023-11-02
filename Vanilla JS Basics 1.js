160f9fc0148a7963322fd4517601177480f720b9
silly_bum <130348695+Sillybum@users.noreply.github.com>
2023-11-02 12:58:40 +0000

    Vanilla JS Basics Assignment
    Javascript calculations and user input

---
+++ Vanilla JS Basics 1.js
@@ -0,0 +1,7 @@
+//Activity 1 - Vanilla JS Basics
+
+console.log(-5 + 8 * 6); // 43
+console.log((55 + 9) % 9); // 1
+console.log(20 + -3 * 5 / 8); // 18.125
+console.log(5 + 15 / 3 * 2 - 8 % 3); //13.0
+ 
---
+++ Vanilla JS Basics 2.js
@@ -0,0 +1,5 @@
+//Activity 2 - Vanilla JS Basics
+
+const NUM1 = prompt("enter the first number", 6); // 6
+const NUM2 = prompt("enter the second number", 2); // 2
+console.log(NUM1 / NUM2); // 3
