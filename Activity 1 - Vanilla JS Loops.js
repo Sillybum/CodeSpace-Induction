d7980a8a178e96283982d2918f0165a64481e2ff
silly_bum <130348695+Sillybum@users.noreply.github.com>
2023-11-09 15:06:36 +0000

    Vanilla JS Loops assginments

---
+++ Activity 1 - Vanilla JS Loops.js
@@ -0,0 +1,7 @@
+for (i = 0; i < 16; i++) { 
+    if (i % 2 === 0) { // using remainder operator to decide if it's even or odd.
+        console.log(i + " is even"); // Putting space between the double quotes so that there's one space between the number and the string.
+    } else {
+        console.log(i + " is odd");
+    }
+}
---
+++ Activity 2 - Vanilla JS Loops.js
@@ -0,0 +1,5 @@
+let num = prompt("enter a number"); // You receive a number here.
+
+for (i = 1; i < 11; i++) { //iterate from 1 through 10
+    console.log(num + " x " + i + " = " + (i * num));
+}
